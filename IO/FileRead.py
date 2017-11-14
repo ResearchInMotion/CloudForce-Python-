0f28f261a83819ab2f622d20ed2bd6d2f7553687
king <king@gmail.com>
2017-11-14 23:37:59 +0530

    file read and write'

---
+++ IO/FileRead.py
@@ -0,0 +1,7 @@
+
+fileopen=open(file='Pythonread.txt',mode='r+')
+print(fileopen.read())
+
+fileopen.write("I am a good boy,sahil")
+print(fileopen.read())
+
---
+++ IO/changefilename()_mkdir()_fileread().py
@@ -0,0 +1,4 @@
+import os
+os.rename("D:\\Pythonread1.txt","D:\\Pythonread2.txt")
+
+os.mkdir("D:\\king")
