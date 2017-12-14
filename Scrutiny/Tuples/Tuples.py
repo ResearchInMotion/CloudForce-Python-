537d16c14513930678725a90c1f36573ce8b6570
Sahil Nagpal <sahilnagpal@Sahils-MacBook-Air.local>
2017-12-14 23:41:57 +0530

    Tuples and moti

---
+++ Scrutiny/Tuples/Tuples.py
@@ -0,0 +1,18 @@
+Tuple=(1,2,3,4,(1,2,3),"sahil","Nikki",-9,9.8)
+
+
+print(Tuple)
+print((Tuple[4][1]))
+
+Tuple1=(1,2,3,"nikki","Rachit")
+print(Tuple[-1])
+print(Tuple[0:2])      #This will only print starting two values not the last one.
+print(Tuple[:])         #printts the whole tuple values
+print(len(Tuple))
+
+tuple=(1,2,3,4)+(12,43)
+print(tuple)
+
+sorttup=(1,2,4,"Sahik")
+for values in sorttup:
+    print(values)
